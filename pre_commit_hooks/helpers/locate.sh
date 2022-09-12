a9442f61af818f753140c862f3ae46aa42a6bcb1
Côme Chilliet <come.chilliet@nextcloud.com>
2022-09-12 15:33:44 +0200

    Support composer bin dir in helpers/locate.sh
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- pre_commit_hooks/helpers/locate.sh
+++ pre_commit_hooks/helpers/locate.sh
@@ -10,7 +10,12 @@ exec_command=""
 # A phar file will need to be called by php
 prefixed_local_command="php $local_command"
 
-if [ -f "$vendor_command" ]; then
+# Path of composer bin dir
+composer_bin_dir=$(composer config bin-dir 2>/dev/null)
+
+if [ -n $composer_bin_dir ] && [ -f "$composer_bin_dir/$global_command" ]; then
+    exec_command="$composer_bin_dir/$global_command"
+elif [ -f "$vendor_command" ]; then
     exec_command=$vendor_command
 elif hash $global_command 2>/dev/null; then
     exec_command=$global_command
@@ -19,6 +24,9 @@ elif [ -f "$local_command" ]; then
 else
     echo -e "${bldred}No valid ${title} found!${txtrst}"
     echo "Please have one available as one of the following:"
+    if [ -n $composer_bin_dir ]; then
+        echo " * $composer_bin_dir/$global_command"
+    fi
     echo " * $local_command"
     echo " * $vendor_command"
     echo " * $global_command"
